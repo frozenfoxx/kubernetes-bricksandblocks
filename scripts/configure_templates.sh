89f4fac719733409844a5e438f93ff9d3ff681f8
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2024-05-15 14:55:04 -0700

    Updated template deployment

--- scripts/configure_templates.sh
+++ scripts/configure_templates.sh
@@ -6,15 +6,44 @@ NFS_INSTANCE_GUID=${NFS_INSTANCE_GUID:-''}
 NFS_HOST=${NFS_HOST:-''}
 ROOT_DIR=${ROOT_DIR:-''}
 
+# Functions
+
+## Export variables to replace in the templates
+export_vars()
+{
+    export NFS_CLUSTER_EXPORT
+    export NFS_INSTANCE_GUID
+    export NFS_HOST
+    export ROOT_DIR
+}
+
+## Unset variables for safety
+unset_vars()
+{
+    unset NFS_CLUSTER_EXPORT
+    unset NFS_INSTANCE_GUID
+    unset NFS_HOST
+    unset ROOT_DIR
+}
+
+## Deploy templates
+deploy_templates()
+{
+    ## Find all .yaml.tmpl files in the repository recursively
+    find "${ROOT_DIR}" -type f -name '*.yaml.tmpl' | while read -r TMPL_FILE; do
+    
+        # Define the output file name by removing the .tmpl extension
+        YAML_FILE="${TMPL_FILE%.tmpl}"
+    
+        # Use envsubst to replace variables in the .tmpl file and create the .yaml file
+        envsubst < "${TMPL_FILE}" > "${YAML_FILE}"
+    
+        echo "Created ${YAML_FILE}"
+    done
+}
+
 # Logic
 
-## Find all .yaml.tmpl files in the repository recursively
-find "${ROOT_DIR}" -type f -name '*.yaml.tmpl' | while read -r TMPL_FILE; do
-    # Define the output file name by removing the .tmpl extension
-    YAML_FILE="${TMPL_FILE%.tmpl}"
-
-    # Use envsubst to replace variables in the .tmpl file and create the .yaml file
-    envsubst < "${TMPL_FILE}" > "${YAML_FILE}"
-
-    echo "Created ${YAML_FILE}"
-done
+export_vars
+deploy_templates
+unset_vars
