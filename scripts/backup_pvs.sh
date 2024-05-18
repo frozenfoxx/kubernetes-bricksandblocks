4d0c581ca283173e8f1d6458850a7308521acb75
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2024-05-17 21:13:03 -0700

    Added backup for pvs

---
+++ scripts/backup_pvs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Variables
+BACKUP_PV_DIR=${BACKUP_PV_DIR:'.'}
+
+# Functions
+
+## Backup
+backup()
+{
+  # Get a list of all persistent volumes
+  PV_LIST=$(kubectl get pv -o go-template="{{range .items}}{{.metadata.name}}{{\n}}{{end}}")
+
+  # Loop through each persistent volume
+  for PV_NAME in ${PV_LIST}; do
+    # Describe the persistent volume
+    PV_DETAILS=$(kubectl get pv ${PV_NAME} -o yaml)
+
+    # Write the description to a YAML file named after the PV
+    echo "${PV_DETAILS}" > "${BACKUP_PV_DIR}/${PV_NAME}.yaml"
+
+    # Inform user
+    echo "Created ${BACKUP_PV_DIR}/${PV_NAME}.yaml"
+  done
+}
+
+# Logic
+
+backup
